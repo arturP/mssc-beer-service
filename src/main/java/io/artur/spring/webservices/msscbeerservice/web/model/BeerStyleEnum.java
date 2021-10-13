8913db6fa80c0fdc3278b38b498a202091cf84ca
Artur Przychodzen <artur2p@gmail.com>
2021-10-13 14:22:07 +0200

    adding web datamodel

---
+++ src/main/java/io/artur/spring/webservices/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package io.artur.spring.webservices.msscbeerservice.web.model;
+
+/**
+ *
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTE, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
