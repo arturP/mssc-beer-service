c7fef4f1f40999ca1e3bba39d9c2499f7d053364
Artur Przychodzen <artur2p@gmail.com>
2021-10-19 15:37:11 +0200

    add ehcache

--- src/main/java/io/artur/spring/webservices/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/io/artur/spring/webservices/msscbeerservice/services/BeerServiceImpl.java
@@ -8,6 +8,7 @@ import io.artur.spring.webservices.msscbeerservice.web.model.BeerDto;
 import io.artur.spring.webservices.msscbeerservice.web.model.BeerPagedList;
 import io.artur.spring.webservices.msscbeerservice.web.model.BeerStyleEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest, Boolean showInventoryOnHand) {
         BeerPagedList beerPagedList;
@@ -68,6 +70,7 @@ public class BeerServiceImpl implements BeerService {
         return beerPagedList;
     }
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false")
     @Override
     public BeerDto getById(UUID beerId, Boolean showInventoryOnHand) {
 
--- src/main/java/io/artur/spring/webservices/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/io/artur/spring/webservices/msscbeerservice/web/model/BeerPagedList.java
@@ -8,13 +8,16 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
+import java.io.Serializable;
 import java.util.List;
 
 /**
  *
  */
 
-public class BeerPagedList extends PageImpl<BeerDto> {
+public class BeerPagedList extends PageImpl<BeerDto> implements Serializable {
+
+    static final long serialVersionUID = 8096931629644913090L;
 
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
     public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
